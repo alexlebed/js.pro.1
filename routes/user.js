a4bbfe4811b78b5d65cab3eb232190c66e6911a9
romashka50 <roma.buchuk@gmail.com>
2015-12-10 18:01:35 +0200

    routing

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,25 @@
 /**
  * Created by romab on 10.12.2015.
  */
-module.exports = function(){
+module.exports = (function(){
+    var express = require('express');
 
-};
+    var userRouter = express.Router();
+
+    userRouter.get('/', function(req, res, next){
+        console.log(req.myVar);
+        res.status(200).send(req.ip);
+    });
+    userRouter.post('/:login/:weight', function(req, res, next){
+        var login = req.params.login;
+        var weight = req.params.weight;
+
+        res.status(200).send({login: login, weight: weight});
+    });
+    userRouter.post('/', function(req, res, next){
+        res.status(200).send(req.body);
+    });
+
+
+    return userRouter;
+})();
--- server.js
+++ server.js
@@ -2,25 +2,13 @@
  * Created by romab on 10.12.2015.
  */
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 3030;
 
-function modifireMidlware(req, res, next){
-    req.myVar = 'sdfsdf';
-    next();
-};
+//'============ Load Routes ==============';
+require('./routes')(app);
+//'============ Load Routes ==============';
 
-app.use(bodyParser.json());
-
-app.post('/user', function(req, res, next){
-    res.status(200).send(req.body);
-});
-app.use(modifireMidlware);
-app.get('/', function(req, res, next){
-    console.log(req.myVar);
-    res.status(200).send(req.ip);
-});
 
 app.listen(port, function(){
     console.log('Server start success = ' + port);
