59ebb873fa6570aa57398a4cb29784eb4e504590
romashka50 <roma.buchuk@gmail.com>
2015-12-10 17:36:24 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+/**
+ * Created by romab on 10.12.2015.
+ */
+var http = require('http');
+
+//in first time please initiate packedge.json --> npm init
+
+//process.env.NODE_ENV = 'production';
+console.log(process.env);
