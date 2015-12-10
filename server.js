0e59b9ce6adc5a688cb91b9c395ef0b8e7d1bf04
romashka50 <roma.buchuk@gmail.com>
2015-12-10 17:42:35 +0200

    firstNodeServer

--- server.js
+++ server.js
@@ -3,7 +3,31 @@
  */
 var http = require('http');
 
-//in first time please initiate packedge.json --> npm init
+var server;
 
-//process.env.NODE_ENV = 'production';
-console.log(process.env);
+server = http.createServer(function(req, res){
+    var url = req.url.split('/');
+    var masterUrl = url[1];
+
+    var enumForUrl = {
+        hello: hello
+    };
+
+    if(enumForUrl[masterUrl]){
+        enumForUrl[masterUrl](res);
+    } else {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end('Page not found');
+    }
+
+
+    function hello(res){
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end('Hello world');
+    }
+
+});
+
+server.listen(3030, function(){
+    console.log('Server is listening on 3030 port');
+});
